7e2a6e1164b145772bc79ba4004d60b4446a092b
HENRY HUMBERTO FLANTRMSKY CARDENAS <hflantrmsky@gmail.com>
2021-06-29 14:58:51 -0500

    Agregar archivos de proyecto.

---
+++ Prueba.Web/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Prueba.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Prueba.API;
+using Prueba.Entities;
+
+namespace Prueba.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        APIWeb api = new APIWeb();
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            Persona t = api.EncontrarPersona(3);
+
+            var model = new IndexModel
+            {
+                PersonaNombre = t
+            };
+
+            return View(model);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Prueba.Web/Models/IndexModel.cs
@@ -0,0 +1,10 @@
+using Prueba.Entities;
+using System;
+
+namespace Prueba.Web.Models
+{
+    public class IndexModel
+    {
+        public Persona PersonaNombre { get; set; }
+    }
+}
